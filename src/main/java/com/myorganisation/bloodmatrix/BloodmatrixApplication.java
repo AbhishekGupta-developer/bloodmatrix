67cd7dbb40bb19ca4333606f725399cea627e458
Abhishek Gupta <Abhishekg.developer@gmail.com>
2024-06-16 20:02:21 +0530

    Add Spring Boot files in repo

---
+++ src/main/java/com/myorganisation/bloodmatrix/BloodmatrixApplication.java
@@ -0,0 +1,13 @@
+package com.myorganisation.bloodmatrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodmatrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodmatrixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myorganisation/bloodmatrix/BloodmatrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myorganisation.bloodmatrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodmatrixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
